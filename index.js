6ef1f0d2087b1a1b4e4dd167a7f1cd0c19c37f95
Thomas <esomly57@gmail.com>
2020-07-05 10:39:28 +0200

    api

--- index.js
+++ index.js
@@ -35,6 +35,12 @@ app.use(
 )
 
 app.use(bodyParser.json());
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+    next();
+});
 app.use(cors())
 
 if (process.env.NODE_ENV === 'production') {
